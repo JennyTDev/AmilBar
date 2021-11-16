574ac6f432681b68297259eb4c3933639437a741
jtradere <jtradere@LT-6R1WMQ2.home>
2021-11-16 14:01:15 +0100

    First commit

---
+++ amilbar/src/main/java/com/amiltone/amilbar/AmilbarApplication.java
@@ -0,0 +1,13 @@
+package com.amiltone.amilbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmilbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmilbarApplication.class, args);
+	}
+
+}
---
+++ amilbar/src/test/java/com/amiltone/amilbar/AmilbarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amiltone.amilbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmilbarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
